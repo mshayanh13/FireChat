f4899b7d77e657aab6c8e1471dcd8913466ef16e
Mohammad Shayan <mohammadhemani@gmail.com>
2020-05-11 06:11:34 +0430

    setup login screen and intro to auto layout

---
+++ FireChat/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,23 @@
+//
+//  RegistrationController.swift
+//  FireChat
+//
+//  Created by Mohammad Shayan on 5/11/20.
+//  Copyright © 2020 Mohammad Shayan. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    //MARK: Properties
+    
+    //MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+    //MARK:
+}
