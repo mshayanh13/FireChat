ea10a9d5295302ef9a9b10b9e76e154cf32ef851
Mohammad Shayan <mohammadhemani@gmail.com>
2020-05-14 20:13:03 +0430

    Setting Up Message Cells

---
+++ FireChat/View/MessageCell.swift
@@ -0,0 +1,65 @@
+//
+//  MessageCell.swift
+//  FireChat
+//
+//  Created by Mohammad Shayan on 5/14/20.
+//  Copyright © 2020 Mohammad Shayan. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UICollectionViewCell {
+    
+    //MARK: Properties
+    
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        return iv
+    }()
+    
+    private let textView: UITextView = {
+        let tv = UITextView()
+        tv.backgroundColor = .clear
+        tv.font = .systemFont(ofSize: 16)
+        tv.isScrollEnabled = true
+        tv.isEditable = false
+        tv.textColor = .white
+        tv.text = "Test Message"
+        return tv
+    }()
+    
+    private let bubbleContainer: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemPurple
+        return view
+    }()
+    
+    //MARK: Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(left: leftAnchor, bottom: bottomAnchor, paddingLeft: 8, paddingBottom: -4)
+        profileImageView.setDimensions(height: 32, width: 32)
+        profileImageView.layer.cornerRadius = 32 / 2
+        
+        addSubview(bubbleContainer)
+        bubbleContainer.layer.cornerRadius = 12
+        bubbleContainer.anchor(top: topAnchor, left: profileImageView.rightAnchor, bottom: bottomAnchor, paddingLeft: 12, paddingBottom: 4)
+        bubbleContainer.widthAnchor.constraint(lessThanOrEqualToConstant: 250).isActive = true
+        
+        addSubview(textView)
+        textView.anchor(top: bubbleContainer.topAnchor, left: bubbleContainer.leftAnchor, bottom: bubbleContainer.bottomAnchor, right: bubbleContainer.rightAnchor, paddingTop: 4, paddingLeft: 12, paddingBottom: 4, paddingRight: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}
